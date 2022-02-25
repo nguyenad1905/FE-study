fb90fe91714adbabd10a620cffb7aeeac496fa81
nguyenad1905 <nguyenad1905@gmail.com>
2022-02-25 09:13:00 +0700

    add slick

---
+++ slick-slider/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    $('.banner-slide').slick({
+        fade: true,
+        dots: true,
+        focusOnSelect: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows: false,
+        infinite: true,
+        // setting - name: setting - value
+    });
+});
